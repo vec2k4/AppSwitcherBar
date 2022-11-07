61d3338b6067bb83e42df39147cade94307f3fcc
Ralf Kruse <ralf.kruse@kannegiesser.de>
2022-11-07 01:08:04 +0100

    add windows taskbar helper

---
+++ net.adamec.ui.AppSwitcherBar/Win32/Services/Taskbar.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+
+namespace net.adamec.ui.AppSwitcherBar.Win32.Services
+{
+    internal class Taskbar
+    {
+        [DllImport("user32.dll")]
+        private static extern int FindWindow(string className, string windowText);
+
+        [DllImport("user32.dll")]
+        private static extern int ShowWindow(int hwnd, int command);
+
+        [DllImport("user32.dll")]
+        public static extern int FindWindowEx(int parentHandle, int childAfter, string className, int windowTitle);
+
+        [DllImport("user32.dll")]
+        private static extern int GetDesktopWindow();
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 1;
+
+        protected static int Handle
+        {
+            get
+            {
+                return FindWindow("Shell_TrayWnd", "");
+            }
+        }
+
+        protected static int HandleOfStartButton
+        {
+            get
+            {
+                int handleOfDesktop = GetDesktopWindow();
+                int handleOfStartButton = FindWindowEx(handleOfDesktop, 0, "button", 0);
+                return handleOfStartButton;
+            }
+        }
+
+        private Taskbar()
+        {
+            // hide ctor
+        }
+
+        public static void Show()
+        {
+            _ = ShowWindow(Handle, SW_SHOW);
+            _ = ShowWindow(HandleOfStartButton, SW_SHOW);
+        }
+
+        public static void Hide()
+        {
+            _ = ShowWindow(Handle, SW_HIDE);
+            _ = ShowWindow(HandleOfStartButton, SW_HIDE);
+        }
+    }
+}
+
